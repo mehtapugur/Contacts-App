99add7d745481df16f63213e156eb91ff556a599
= <=>
2021-10-11 00:33:02 +0300

    Remove logos

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 function Form() {
-  return <div>form</div>;
+  const [form, setForm] = useState({ fullname: "", phone_number: "" });
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
+      </div>
+
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
 }
 
 export default Form;
